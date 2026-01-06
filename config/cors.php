7e6b85715afadcb61db63761eafd5c2fe0b627f1
it <duircianos@yandex.ru>
2026-01-06 13:16:11 +0400

    somechanges

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [$_ENV['FRONT_URL']],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
